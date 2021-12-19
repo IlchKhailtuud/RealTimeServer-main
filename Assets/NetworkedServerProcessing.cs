e9731babd63bcdcb4bdc9ab1ca232b4851d8daa3
Fnu Yiliqi <khai.ilch167@gmail.com>
2021-12-19 15:57:32 -0500

    Implemented real time position update

---
+++ Assets/NetworkedServerProcessing.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static public class NetworkedServerProcessing
+{
+
+    #region Send and Receive Data Functions
+    static public void ReceivedMessageFromClient(string msg, int clientConnectionID)
+    {
+        Debug.Log("msg received = " + msg + ".  connection id = " + clientConnectionID);
+
+        string[] csv = msg.Split(',');
+        int signifier = int.Parse(csv[0]);
+
+        if (signifier == ClientToServerSignifiers.BalloonClicked)
+        {
+            gameLogic.BalloonWasClicked(int.Parse(csv[1]));
+        }
+        // else if (signifier == ClientToServerSignifiers.asd)
+        // {
+
+        // }
+
+        //gameLogic.DoSomething();
+    }
+    static public void SendMessageToClient(string msg, int clientConnectionID)
+    {
+        networkedServer.SendMessageToClient(msg, clientConnectionID);
+    }
+
+    #endregion
+
+    #region Connection Events
+
+    static public void ConnectionEvent(int clientConnectionID)
+    {
+        Debug.Log("New Connection, ID == " + clientConnectionID);
+        gameLogic.AddConnectedClient(clientConnectionID);
+    }
+    static public void DisconnectionEvent(int clientConnectionID)
+    {
+        Debug.Log("New Disconnection, ID == " + clientConnectionID);
+        gameLogic.RemoveConnectedClient(clientConnectionID);
+    }
+
+    #endregion
+
+    #region Setup
+    static NetworkedServer networkedServer;
+    static GameLogic gameLogic;
+
+    static public void SetNetworkedServer(NetworkedServer NetworkedServer)
+    {
+        networkedServer = NetworkedServer;
+    }
+    static public NetworkedServer GetNetworkedServer()
+    {
+        return networkedServer;
+    }
+    static public void SetGameLogic(GameLogic GameLogic)
+    {
+        gameLogic = GameLogic;
+    }
+
+    #endregion
+}
+
+#region Protocol Signifiers
+static public class ClientToServerSignifiers
+{
+    public const int BalloonClicked = 1;
+}
+
+static public class ServerToClientSignifiers
+{
+    public const int BalloonSpawned = 1;
+    public const int BalloonPopped = 1;
+}
+
+#endregion
+
